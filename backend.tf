375673b96a931cb2cf35e1113434440056022efd
mohammad1774 <mohammad.ahmed1774@gmail.com>
2025-09-25 16:55:26 -0400

    bootstrapped the backend of tfstate

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
-    storage_account_name = "tfstateprod1"
+    storage_account_name = "tfstateprodtest1"
     container_name       = "tfstate"
     key                  = "infra.tfstate"
   }
