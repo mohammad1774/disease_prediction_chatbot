322546a80894c357ad133201f70bccc9f61a065b
mohammad1774 <mohammad.ahmed1774@gmail.com>
2025-09-25 22:35:10 -0400

    fixing the .gitignore

--- resources.tf
+++ resources.tf
@@ -1,4 +1,18 @@
-resource "azurerm_resource_group" "example" {
-    name = "example-resources-test"
-    location = "canadacentral"
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "example-resources"
+  location = "canadacentral"
+  tags = {
+    environmet = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location = azurerm_resource_group.mtc-rg.location
+  address_space = ["10.123.0.0/16"]
+
+  tags = {
+    environmet = "dev"
+  }
 }
