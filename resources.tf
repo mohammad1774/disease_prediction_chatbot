ce7d8314a3c99cf91bc88e2e02ec17a1009e10d3
mohammad1774 <mohammad.ahmed1774@gmail.com>
2025-09-25 23:07:29 -0400

    add security group

--- resources.tf
+++ resources.tf
@@ -7,10 +7,10 @@ resource "azurerm_resource_group" "mtc-rg" {
 }
 
 resource "azurerm_virtual_network" "mtc-vn" {
-  name = "mtc-network"
+  name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
-  location = azurerm_resource_group.mtc-rg.location
-  address_space = ["10.123.0.0/16"]
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
 
   tags = {
     environmet = "dev"
@@ -18,8 +18,19 @@ resource "azurerm_virtual_network" "mtc-vn" {
 }
 
 resource "azurerm_subnet" "mtc-subnet" {
-  name = "mtc-subnet"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-  address_prefixes = ["10.123.1.0/24"]
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environmet = "dev"
+  }
+
 }
