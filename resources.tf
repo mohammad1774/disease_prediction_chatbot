308dcd3409f0e119ea2a05a779da6750583c16cc
mohammad1774 <mohammad.ahmed1774@gmail.com>
2025-09-25 20:50:31 -0400

    resource group name changed

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_test" "example" {
-    name = "example-resources-name"
+resource "azurerm_resource_group" "example" {
+    name = "example-resources-test"
     location = "canadacentral"
 }
