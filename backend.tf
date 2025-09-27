07a9e1f71d46b69991f0b29de23b198051c067df
mohammad1774 <mohammad.ahmed1774@gmail.com>
2025-09-26 22:13:11 -0400

    end to end workflow

--- backend.tf
+++ backend.tf
@@ -13,6 +13,4 @@ terraform {
     }
   }
 }
-provider "azurerm" {
-  features {}
-}
+
---
+++ vars.tf
@@ -0,0 +1,17 @@
+output "acr_login_server" {
+    value = azurerm_container_registry.prediction-acr.login_server
+}
+output "acr_admin_username" {
+    value = azurerm_container_registry.prediction-acr.admin_username
+}
+
+output "acr_admin_password" {
+    value = azurerm_container_registry.prediction-acr.admin_password
+    sensitive = true
+}
+
+output "aks_kube_config" {
+  value     = azurerm_kubernetes_cluster.prediction-aks.kube_config_raw
+  sensitive = true
+}
+
